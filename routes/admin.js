4b5143fb751a09baf6f9a505ba8d63948778bb67
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2024-07-02 01:24:35 +0530

    connecting and saving data on db

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 
-const { getAddProducts, getPostProducts } = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', getAddProducts);
+router.get('/add-product', adminController.getAddProduct);
+
+// // /admin/products => GET
+// router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', getPostProducts);
+router.post('/add-product', adminController.postAddProduct);
 
-exports.routes = router;
+// router.get('/edit-product/:productId', adminController.getEditProduct);
+
+// router.post('/edit-product', adminController.postEditProduct);
+
+// router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
